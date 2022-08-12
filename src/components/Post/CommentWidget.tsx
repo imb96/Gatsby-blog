78d4532e6df63696148973a8df3d01a500f0c759
Kimminjae <kimminjae7267@daum.net>
2022-08-12 16:58:26 +0900

    repo change

--- src/components/Post/CommentWidget.tsx
+++ src/components/Post/CommentWidget.tsx
@@ -2,7 +2,7 @@ import React, { createRef, FunctionComponent, useEffect } from 'react'
 import styled from '@emotion/styled'
 
 const src = 'https://utteranc.es/client.js'
-const repo = 'imb96/Minlog' // 자신 계정의 레포지토리로 설정
+const repo = 'imb96/imb96.github.io' // 자신 계정의 레포지토리로 설정
 
 type UtterancesAttributesType = {
   src: string
