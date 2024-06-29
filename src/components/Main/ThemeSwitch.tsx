38e65fdcd3c57e9be500d398e1f9c0252772a303
김민재 <kimminje7267@naver.com>
2024-06-29 17:03:01 +0900

    chore: package manager change

--- src/components/Main/ThemeSwitch.tsx
+++ src/components/Main/ThemeSwitch.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import { ThemeToggler } from 'gatsby-plugin-dark-mode'
-import DarkModeToggle from 'react-dark-mode-toggle'
+// import React from 'react'
+// import { ThemeToggler } from 'gatsby-plugin-dark-mode'
+// import DarkModeToggle from 'react-dark-mode-toggle'
 
-class ThemeSwitch extends React.Component {
-  render() {
-    return (
-      <ThemeToggler>
-        {({ theme, toggleTheme }) => (
-          <DarkModeToggle
-            onChange={e => toggleTheme(e ? 'dark' : 'light')}
-            checked={theme === 'dark'}
-            size={60}
-            speed={3}
-          />
-        )}
-      </ThemeToggler>
-    )
-  }
-}
+// class ThemeSwitch extends React.Component {
+//   render() {
+//     return (
+//       <ThemeToggler>
+//         {({ theme, toggleTheme }) => (
+//           <DarkModeToggle
+//             onChange={e => toggleTheme(e ? 'dark' : 'light')}
+//             checked={theme === 'dark'}
+//             size={60}
+//             speed={3}
+//           />
+//         )}
+//       </ThemeToggler>
+//     )
+//   }
+// }
 
-export default ThemeSwitch
+// export default ThemeSwitch
